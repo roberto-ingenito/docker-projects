fde716dd8ac05fa10b7043d54046727d3d222c13
Roberto Ingenito <robe.ingenito@gmail.com>
2026-03-03 15:52:56 +0100

    modificato docker compose

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -8,20 +8,20 @@ sudo apt update
 sudo apt install certbot -y
 
 # Crea le directory necessarie
-mkdir -p nginx/certbot/conf
-mkdir -p nginx/certbot/www
+mkdir -p main-nginx/certbot/conf
+mkdir -p main-nginx/certbot/www
 
 # Genera i certificati (certbot usa la porta 80)
 sudo certbot certonly --standalone -d roberto-ingenito.ddns.net --email robe.ingenito@gmail.com --agree-tos --no-eff-email
 
 # Copia i certificati nella cartella che nginx si aspetta
-sudo cp -r /etc/letsencrypt/live nginx/certbot/conf/
-sudo cp -r /etc/letsencrypt/archive nginx/certbot/conf/
-sudo cp -r /etc/letsencrypt/renewal nginx/certbot/conf/
+sudo cp -r /etc/letsencrypt/live main-nginx/certbot/conf/
+sudo cp -r /etc/letsencrypt/archive main-nginx/certbot/conf/
+sudo cp -r /etc/letsencrypt/renewal main-nginx/certbot/conf/
 
 # Correggi i permessi
-sudo chown -R $USER:$USER nginx/certbot/conf
-sudo chmod -R 755 nginx/certbot/conf
+sudo chown -R $USER:$USER main-nginx/certbot/conf
+sudo chmod -R 755 main-nginx/certbot/conf
 
 echo "✅ Certificati generati e configurati!"
 echo "I certificati si rinnoveranno automaticamente tramite il container certbot"
