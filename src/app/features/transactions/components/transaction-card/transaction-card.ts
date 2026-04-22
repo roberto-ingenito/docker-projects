2ed9cf48fb1bb3a9bfd36cd7e38b981c6160a5ca
Roberto Ingenito <robe.ingenito@gmail.com>
2026-04-22 12:17:30 +0200

    feat: enhance transaction card with category and date display, improve layout and styling

--- src/app/features/transactions/components/transaction-card/transaction-card.ts
+++ src/app/features/transactions/components/transaction-card/transaction-card.ts
@@ -1,7 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Transaction, TransactionType } from '../../../../../lib/types/transaction';
 import { Button } from '../../../../shared/components/button/button';
 import { Icon } from '../../../../shared/components/icon/icon';
+import { CategoriesStore } from '../../../../core/services/categories.store';
 
 @Component({
   selector: 'app-transaction-card',
@@ -10,7 +11,24 @@ import { Icon } from '../../../../shared/components/icon/icon';
   styleUrl: './transaction-card.scss',
 })
 export class TransactionCard {
+  categoriesStore = inject(CategoriesStore);
+
   transaction = input.required<Transaction>();
 
+  category = computed(() =>
+    this.categoriesStore //
+      .state()
+      ?.find((x) => x.categoryId === this.transaction().categoryId),
+  );
+
   isIncome = computed(() => this.transaction().type === TransactionType.Income);
+
+  formatDate(dateString: string) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('it-IT', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    });
+  }
 }
