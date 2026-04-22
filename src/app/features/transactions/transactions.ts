fc09b0987cd3c95cb49a20c3024180990be93c3f
Roberto Ingenito <robe.ingenito@gmail.com>
2026-04-22 18:25:13 +0200

    feat(transactions): add statistic cards (da completare)

--- src/app/features/transactions/transactions.ts
+++ src/app/features/transactions/transactions.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Button } from '../../shared/components/button/button';
 import { TransactionsStore } from '../../core/services/transactions.store';
 import { TransactionCard } from './components/transaction-card/transaction-card';
+import { Icon } from '../../shared/components/icon/icon';
 
 @Component({
   selector: 'app-transactions',
-  imports: [Button, TransactionCard],
+  imports: [Button, TransactionCard, Icon],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
