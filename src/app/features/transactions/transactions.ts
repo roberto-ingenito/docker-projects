4b8cc60f7bc9f4c17fcb1e506ce3d5a99ef07499
Roberto Ingenito <robe.ingenito@gmail.com>
2026-04-22 18:38:09 +0200

    feat(transactions): display formatted income, expense, and balance values in statistic cards

--- src/app/features/transactions/transactions.ts
+++ src/app/features/transactions/transactions.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { Button } from '../../shared/components/button/button';
 import { TransactionsStore } from '../../core/services/transactions.store';
 import { TransactionCard } from './components/transaction-card/transaction-card';
 import { Icon } from '../../shared/components/icon/icon';
+import { TransactionType } from '../../../lib/types/transaction';
+import { AuthStore } from '../../core/services/auth.store';
 
 @Component({
   selector: 'app-transactions',
@@ -13,4 +15,25 @@ import { Icon } from '../../shared/components/icon/icon';
 })
 export class Transactions {
   transactionsStore = inject(TransactionsStore);
+  authStore = inject(AuthStore);
+
+  userCurrency = computed(() => this.authStore.state()?.user.currency ?? 'EUR');
+
+  statistics = computed(() => {
+    const income = this.transactionsStore
+      .state()
+      ?.filter((t) => t.type === TransactionType.Income)
+      .reduce((sum, t) => sum + t.amount, 0);
+
+    const expense = this.transactionsStore
+      .state()
+      ?.filter((t) => t.type === TransactionType.Expense)
+      .reduce((sum, t) => sum + t.amount, 0);
+
+    return {
+      income: income ?? 0,
+      expense: expense ?? 0,
+      balance: (income ?? 0) - (expense ?? 0),
+    };
+  });
 }
